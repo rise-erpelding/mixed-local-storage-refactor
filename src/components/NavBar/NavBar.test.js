f9c8c469e174366a0af4c35f4cbbef83ec0ed15a
Rise <rise.erpelding@gmail.com>
2020-07-28 09:58:45 -0500

    smoke test for NavBar

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import NavBar from './NavBar';
+
+it('renders without crashing', () => {
+  const nav = document.createElement('nav');
+  ReactDOM.render(<BrowserRouter><NavBar /></BrowserRouter>, nav);
+  ReactDOM.unmountComponentAtNode(nav);
+});
