3ef392c8cf7898e875c12c6108ad90ac1c50036b
Rise <rise.erpelding@gmail.com>
2020-07-27 22:06:46 -0500

    setting up initial styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import LandingPage from '../routes/Landing/LandingPage';
+import LandingPage from '../../routes/Landing/LandingPage';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Switch>
           <Route 
             exact
