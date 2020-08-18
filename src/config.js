09bf5a5190903c834b4d013d51b05c8b01fb63f9
Rise <rise.erpelding@gmail.com>
2020-08-17 19:37:59 -0500

    added service files and initial config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: 'https://fathomless-hamlet-74680.herokuapp.com/api',
+  TOKEN_KEY: 'mixed-client-auth-token',
+};
---
+++ src/services/mixed-api-service.js
@@ -0,0 +1,139 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const MixedApiService = {
+  getClassesForTeacher() {
+    return fetch(`${config.API_ENDPOINT}/classes/teacher`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => Promise.reject(error));
+        }
+        return res.json();
+      });
+  },
+  getGroupingsForTeacher() {
+    return fetch(`${config.API_ENDPOINT}/groupings/teacher`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => Promise.reject(error));
+        }
+        return res.json();
+      });
+  },
+  insertNewClass(newClass) {
+    return fetch(`${config.API_ENDPOINT}/classes`, {
+      method: 'POST',
+      body: JSON.stringify(newClass),
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+        return res.json();
+      });
+  },
+  insertNewGrouping(newGrouping) {
+    return fetch(`${config.API_ENDPOINT}/groupings`, {
+      method: 'POST',
+      body: JSON.stringify(newGrouping),
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+        return res.json();
+      });
+  },
+  editClass(classId, updatedClass) {
+    return fetch(`${config.API_ENDPOINT}/classes/${classId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updatedClass),
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+  },
+  editGrouping(groupingId, updatedGrouping) {
+    return fetch(`${config.API_ENDPOINT}/groupings/${groupingId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updatedGrouping),
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+  },
+  deleteClass(classId) {
+    return fetch(`${config.API_ENDPOINT}/classes/${classId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+  },
+  deleteGrouping(groupingId) {
+    return fetch(`${config.API_ENDPOINT}/groupings/${groupingId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+  }
+}
+
+export default MixedApiService;
