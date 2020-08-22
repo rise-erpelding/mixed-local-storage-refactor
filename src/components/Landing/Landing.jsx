cdebacbf41cdd8956434d6a402746bfe4c2bc58e
Rise <rise.erpelding@gmail.com>
2020-08-21 22:01:40 -0500

    added styles

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,13 +6,19 @@ const Landing = (props) => {
     return (
       <section className="landing">
         <h1>
-          <span className="landing__orange">M</span>
-          <span className="landing__yellow">i</span>
-          <span className="landing__orange">x</span>
+          <span className="landing__orange">Mix</span>
           {' '}
           <span className="landing__yellow">ED</span>
         </h1>
-        <h2 className="landing__yellow">The stress-free way to make classroom groups</h2>
+        <h2>
+          <span className="landing__orange">The</span>
+          {' '}
+          <span className="landing__yellow">stress-free</span>
+          {' '}
+          <span className="landing__orange">way to make</span>
+          {' '}
+          <span className="landing__yellow">classroom groups</span>
+        </h2>
         <div className="landing__buttons">
           <button type="button" onClick={props.handleOnboarding}>
             See how it works
--- src/components/UserOnboarding/UserOnboarding.jsx
+++ src/components/UserOnboarding/UserOnboarding.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import GroupGeneratorScreen from '../../images/group-generator-screen.png';
+import SavedGroupsScreen from '../../images/saved-groups-screen.png';
 import './UserOnboarding.css';
 
 class UserOnboarding extends Component {
@@ -7,36 +9,29 @@ class UserOnboarding extends Component {
     return (
       <section ref={this.props.passRef} className="user-onboarding">
         <div className="user-onboarding__intro">
-          [placeholder for image of groupings]
           <h3>MixEd lets teachers generate groups based on data.</h3>
         </div>
         <div className="user-onboarding__data-entry">
           <h3>
-            Enter the data
+            Enter the data into the group generator
           </h3>
-          [placeholder for image of sample data being entered into generator]
+          <img
+            src={GroupGeneratorScreen}
+            className="user-onboarding__data-entry--img"
+            alt="screen shot of group generator"
+          />
           <p>First, enter the data for grouping students. At a minimum, you need to provide a student alias (such as a name, number, etc.) to identify the student. You may also add categories that you want to be taken into account when making groups--for example, student grades, personality types, learning styles, topics of interest--any value that you might want to use to categorize students.</p>
           <p>Put one value on each line. The order of the values MUST correspond to the order in which students are inputted. We recommend saving your categories on a spreadsheet and copying/pasting from a spreadsheet.</p>
         </div>
-        <div className="user-onboarding__generate-groups">
-          <h3>
-            Generate groups
-          </h3>
-          [placeholder for generate button]
-          <p>Once you have entered all your data and picked all necessary criteria, click on the Generate button to generate your groups.</p>
-        </div>
-        <div className="user-onboarding__save-groups">
-          <h3>
-            Save groups to view them again later
-          </h3>
-          [placeholder for editing image]
-          <p>If you’re not happy with your groups, you can edit them to your satisfaction. If you want to save your groups to your account (must be logged in), click Save. </p>
-        </div>
-        <div className="user-onboarding__organize-groups">
+        <div className="user-onboarding__saved-groups">
           <h3>
             Organize groupings by class
           </h3>
-          [placeholder for teacher data screen]
+          <img
+            src={SavedGroupsScreen}
+            className="user-onboarding__saved-groups--img"
+            alt="screen shot of group generator"
+          />
           <p>You can save your groups to an existing class or create a new class for the grouping to belong to, then view your groupings organized by class.</p>
         </div>
         <div className="user-onboarding__buttons">
