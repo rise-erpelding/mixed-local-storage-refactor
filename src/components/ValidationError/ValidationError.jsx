05b0544f994dc52255163c5ab5a12ec912ac207b
Rise <rise.erpelding@gmail.com>
2020-08-26 09:43:48 -0500

    added proptypes

--- src/components/ValidationError/ValidationError.jsx
+++ src/components/ValidationError/ValidationError.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import './ValidationError.css';
 
 const ValidationError = (props) => {
@@ -16,10 +16,10 @@ const ValidationError = (props) => {
 
 export default ValidationError;
 
-// ValidationError.defaultProps = {
-//   message: '',
-// };
+ValidationError.defaultProps = {
+  message: '',
+};
 
-// ValidationError.propTypes = {
-//   message: PropTypes.string,
-// };
+ValidationError.propTypes = {
+  message: propTypes.string,
+};
