295cac8be7353031a28d2cff22518a4de7c0db71
Rise Erpelding <rise@heysparkbox.com>
2023-02-03 12:27:59 -1000

    refactor: extract aliases textarea

---
+++ src/components/MakeGroupsForm/src/form-inputs/textarea-input.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+export const TextAreaInputSection = ({ label, name, onChange, placeholderText, value }) => {
+  return (
+    <section>
+      <div>
+        <label htmlFor={name}>{label}</label>
+      </div>
+      <textarea
+        className="make-groups-page__form--textarea"
+        columns="20"
+        id={name}
+        name={name}
+        onChange={onChange}
+        placeholder={placeholderText}
+        rows="26"
+        value={value}
+      />
+    </section>
+  );
+};
+
+
+TextAreaInputSection.propTypes = {
+  label: propTypes.string,
+  max: propTypes.number,
+  min: propTypes.number,
+  name: propTypes.string,
+  placeholderText: propTypes.string,
+  onChange: propTypes.func,
+  value: propTypes.string,
+};
